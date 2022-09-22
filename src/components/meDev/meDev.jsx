f1dbece1b6102e0c238152f9d42a2e9e23f15746
Senails <76501177+Senails@users.noreply.github.com>
2022-09-22 23:23:06 +0300

    add ancers

--- src/components/meDev/meDev.jsx
+++ src/components/meDev/meDev.jsx
@@ -2,8 +2,8 @@ import './style.scss';
 
 export default function Devme(){
     return <div className="aboutme">
-        <a href="/1312"></a>
-        <a href="/1312"></a>
-        <a href="/1312"></a>
+        <a href="https://www.figma.com/file/WLelggD9pZ8FOIQ4QDqlcC/Untitled?node-id=0%3A1"></a>
+        <a href="https://github.com/Senails/my-app"></a>
+        <a href="https://senails.github.io/Senails/"></a>
     </div>
 }
